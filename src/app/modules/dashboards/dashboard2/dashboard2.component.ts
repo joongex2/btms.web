8c9e70e813cb48b1de07dcb556aa985dd06ce671
joongex3 <s.auengsirirat@gmail.com>
2023-10-10 21:22:54 +0700

    fix: width/height ratio

--- src/app/modules/dashboards/dashboard2/dashboard2.component.ts
+++ src/app/modules/dashboards/dashboard2/dashboard2.component.ts
@@ -160,7 +160,7 @@ export class Dashboard2Component implements OnInit, OnChanges {
     this.barChart2Options = {
       chart: {
         type: 'bar',
-        height: 550
+        height: 'auto'
       },
       plotOptions: {
         bar: {
